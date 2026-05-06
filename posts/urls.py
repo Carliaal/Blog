1cbade60f4b8dfeddf2b75f5cbc2333456c2673d
Carliaal <carlagarciapachecotf@gmail.com>
2026-05-06 10:50:16 +0100

    feat #7: Posts forms add_form and edit_form views, urls and templates

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
+    path('add/', views.add_post, name="post-add"),
     path('<slug:post_slug>/', views.post_detail, name='post-detail'),
+    path('<slug:post_slug>/edit', views.edit_post, name='post-edit')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
 
 from .models import Post
+from .forms import AddPostForm, EditPostForm
+
 
 
 def post_list(request):
@@ -22,3 +25,24 @@ def post_detail(request, post_slug: str):
         'posts/post/detail.html',
         {'post': post},
     )
+
+def add_post(request):
+    if request.method == 'POST':
+        if (form := AddPostForm(request.POST)).is_valid():
+            form.save()
+            return redirect('posts:post-list')
+    else:
+        form = AddPostForm()
+    return render(request, 'posts/post/add.html', {'form': form})
+
+def edit_post(request, post_slug: str):
+    post = Post.objects.get(slug=post_slug)
+    if request.method == 'POST':
+        if (form := EditPostForm(request.POST, instance=post)).is_valid():
+            post = form.save(commit=False)
+            post.slug = slugify(post.title)
+            post.save()
+            return redirect('posts:post-list')
+    else:
+        form = EditPostForm(instance=post)
+    return render(request, 'posts/post/edit.html', {'post': post, 'form': form})
