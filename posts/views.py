0537ce62348ac527ff1d2abb1de8e6b90ca6ceb8
Carliaal <carlagarciapachecotf@gmail.com>
2026-05-06 09:53:33 +0100

    feat #5: post_list and post_detail views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
 
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        'posts/post/list.html',
+        {'posts': posts},
+    )
+
+def post_detail(request, post_slug: str):
+    try:
+        post = Post.objects.get(slug=post_slug)
+    except Post.DoesNotExist:
+        return HttpResponse(f'Post with slug "{post_slug}" does not exist!')
+    return render(
+        request,
+        'posts/post/detail.html',
+        {'post': post},
+    )
