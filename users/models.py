504bd08fc230136b519a59c3891f018e80e82a82
Carliaal <carlagarciapachecotf@gmail.com>
2026-05-07 13:53:43 +0100

    feat: users views

--- users/models.py
+++ users/models.py
@@ -6,6 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'{self.user.username} profile'
